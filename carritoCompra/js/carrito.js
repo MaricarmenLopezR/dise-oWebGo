124affc55d337c82b63ef0b1e570a8c5b65b1ba0
Maricarmen LR <lopezromeromaricarmen03@gmail.com>
2022-06-09 18:47:56 -0500

    Respaldo 03 9/06/2022

--- carritoCompra/js/carrito.js
+++ carritoCompra/js/carrito.js
@@ -3,7 +3,11 @@ const carrito = document.querySelector('#carrito');
 const listaProductos = document.querySelector('#lista_productos');
 const contenedorCarrito = document.querySelector('#lista_carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar_carrito'); 
+const precioTotal = document.querySelector('#priceTotal');
+const contadorProducto = document.querySelector('.count_product');
+let totalCard =0;
 let articulosCarrito = [];
+let countProduct =0;
 
 // Listeners
 cargarEventListeners();
@@ -31,6 +35,8 @@ function leerDatosProducto(producto) {
          id: producto.querySelector('a').getAttribute('data-id'), 
          cantidad: 1
     }
+    totalCard = parseFloat(totalCard) + parseFloat(infoProducto.precio);
+    totalCard = totalCard.toFixed(2);
 
     if( articulosCarrito.some( producto => producto.id === infoProducto.id ) ) { 
          const productos = articulosCarrito.map( producto => {
@@ -44,6 +50,7 @@ function leerDatosProducto(producto) {
          articulosCarrito = [...productos];
     }  else {
          articulosCarrito = [...articulosCarrito, infoProducto];
+         countProduct++;
     }
     carritoHTML();
 }
@@ -53,36 +60,47 @@ function eliminarProducto(e) {
     e.preventDefault();
     if(e.target.classList.contains('borrar_producto') ) {
          const productoId = e.target.getAttribute('data-id')
+         articulosCarrito.forEach(value =>{
+               if (value.id ==productoId) {
+                    let priceReduce = parseFloat(value.precio) * parseFloat(value.cantidad);
+                    totalCard = totalCard - priceReduce;
+                    totalCard = totalCard.toFixed(2);
+               }
+         });
          articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
          carritoHTML();
+         countProduct--;
     }
 }
 
 
-function carritoHTML() {
+function carritoHTML(){
 
     vaciarCarrito();
 
-    articulosCarrito.forEach(producto => {
+    articulosCarrito.forEach((producto) => {
          const row = document.createElement('tr');
          row.innerHTML = `
               <td>  
                    <img src="${producto.imagen}" width=100>
               </td>
               <td>${producto.titulo}</td>
-              <td>${producto.precio}</td>
+              <td>s/${producto.precio}</td>
               <td>${producto.cantidad} </td>
               <td>
                    <a href="#" class="borrar_producto" data-id="${producto.id}">X</a>
               </td>
          `;
          contenedorCarrito.appendChild(row);
+         precioTotal.innerHTML = totalCard;
+         contadorProducto.innerHTML = countProduct;
     });
-
 }
 
 function vaciarCarrito() {
     while(contenedorCarrito.firstChild) {
          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+         precioTotal.innerHTML = '0';
+         contadorProducto.innerHTML = '0';
      }
 }
